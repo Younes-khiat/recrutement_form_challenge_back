85804d593819007f2978925582aed896ca7525e2
Younes-khiat <Khiatyounes27@gmail.com>
2024-08-31 01:30:17 +0100

    backk

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(`${process.env.MONGODB_URI}`);
 const app = express();
 
 app.use(cors({
