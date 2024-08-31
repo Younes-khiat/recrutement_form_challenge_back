c5ef9e5e33c89c8523a280bcf4ad9c86d5f98c42
Younes-khiat <Khiatyounes27@gmail.com>
2024-08-31 01:58:38 +0100

    backend done

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const userRouter = require('./routes/users');
+
 mongoose.connect(`${process.env.MONGODB_URI}`);
+
 const app = express();
+const port = 3002;
 
 app.use(cors({
   origin: 'http://localhost:3000' ,
@@ -12,8 +16,6 @@ app.use(cors({
 }));
 app.use((bodyParser.json()));
 
-const userRouter = require('./routes/users');
-const port = 3002;
 app.use(express.json());
 app.use('/',userRouter);
 app.listen(port, () => {
--- routes/users.js
+++ routes/users.js
@@ -4,24 +4,13 @@ const users = require('../models/users');
 
 router.post('/register', async (req, res) => {
   console.log(12);
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // Replace with your frontend's origin
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Methods', 'POST');
     try {
-      console.log(11);
-      console.log(req.body);
       const userData = (req.body);
-      console.log(66);
-      // const parsedData = JSON.stringify(userData);
-      console.log(13);
-
-      console.log(userData);
-      console.log(15);
-console.log(22);
       const user = new users(userData);
-      console.log(25);
       await user.save();
-console.log(33);
       res.status(201).json(user);
     } catch (err) {
       console.error(err);
