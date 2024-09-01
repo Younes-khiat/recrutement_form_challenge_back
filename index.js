74cbf7dac6f2da35cb9fc376a7a12434bb206757
Younes-khiat <Khiatyounes27@gmail.com>
2024-09-02 00:48:52 +0100

    not done 5

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const userRouter = require('./routes/users');
-
-mongoose.connect(`${process.env.MONGODB_URI}`);
 
 const app = express();
-const port = 3002;
-
-app.use(cors({
-  origin: 'https://recrutement-form-challenge-back-2.onrender.com' ,
-
-}));
-app.use((bodyParser.json()));
+const port = process.env.PORT || 3002;  // Use environment variable for port
 
 app.use(express.json());
-app.use('/',userRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the API');
+});
+
 app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
+  console.log(`Server listening on port ${port}`);
+});
