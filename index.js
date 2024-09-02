c2b17145db92b6efe27707f91d4b4be64db4bc82
Younes-khiat <Khiatyounes27@gmail.com>
2024-09-02 01:10:42 +0100

    not done 7

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const cors = require('cors');
 require('dotenv').config();
 const userRouter = require('./routes/users');
 
-mongoose.connect(`${process.env.MONGODB_URI}`);
-
+mongoose.connect(`${process.env.MONGODB_URI}`).catch(error => {
+  console.error('Error connecting to MongoDB:', error);
+});
 const app = express();
 const port = 3002;
 const allowedOrigins = ['https://recrutement-form-challenge-front.vercel.app'];
