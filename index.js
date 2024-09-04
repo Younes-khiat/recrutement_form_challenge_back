12771bd315b976a2052d2e33dc65d2819bb3e622
Younes-khiat <Khiatyounes27@gmail.com>
2024-09-04 10:16:29 +0100

    not done8

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-
-
-
-  const express = require('express');
+const express = require('express');
+// const ejs = require('ejs');
 
 const app = express();
-const port = process.env.PORT || 3002;  // Use environment variable for port
+const port = process.env.PORT || 3002;
 
 app.use(express.json());
 
+// // Configure EJS
+// app.set('view engine', 'ejs');
+// app.set('views', 'views');
+
 app.get('/', (req, res) => {
-  res.send('Welcome to the API');
+    res.render('index', { title: 'Welcome to the API' });
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+    console.log(`Server listening on port ${port}`);
 });
