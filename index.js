0c8c0f6a7ef97e04580debabec1079879c89ee85
Younes-khiat <Khiatyounes27@gmail.com>
2024-09-04 12:20:57 +0100

    not done15

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+
+
+
 const express = require('express');
-const path = require('path'); // Make sure to include path
-const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const userRouter = require('./routes/users');
+
+mongoose.connect(`${process.env.MONGODB_URI}`).catch(error => {
+  console.error('Error connecting to MongoDB:', error);
+});
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
@@ -8,10 +17,22 @@ console.log('Views directory:', path.join(__dirname, 'views'));
 
 app.set('views', path.join(__dirname, 'views')); // Explicitly set views directory
 
-app.get('/', (req, res) => {
-    res.render('index'); // Renders views/index.ejs
-});
+const app = express();
+const port = 3002;
+const allowedOrigins = ['https://recrutement-form-challenge-front.vercel.app'];
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
 
-app.listen(3002, () => {
-    console.log('Server is running on port 3000');
-});
+}));
+
+
+app.use(express.json());
+
+app.use('/',userRouter);
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
