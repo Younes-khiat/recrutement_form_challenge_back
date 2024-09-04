e8f4d9b6efafd1eea8c4b5f6ca192a9b2f393b9c
Younes-khiat <Khiatyounes27@gmail.com>
2024-09-04 12:38:22 +0100

    not done18

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ mongoose.connect(`${process.env.MONGODB_URI}`).catch(error => {
 
 const app = express();
 const port = 3002;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+console.log('Views directory:', path.join(__dirname, 'views'));
+
+app.set('views', path.join(__dirname, 'views')); // Explicitly set views directory
+
+
 const allowedOrigins = ['https://recrutement-form-challenge-front.vercel.app'];
 app.use(cors({
   origin: allowedOrigins,
@@ -22,11 +30,6 @@ app.use(cors({
 
 }));
 
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
-console.log('Views directory:', path.join(__dirname, 'views'));
-
-app.set('views', path.join(__dirname, 'views')); // Explicitly set views directory
 
 
 
