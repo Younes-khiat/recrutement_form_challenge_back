e9d7a17483d0081cd9e010d78307a6524e6ec3a7
Younes-khiat <Khiatyounes27@gmail.com>
2024-09-04 11:43:26 +0100

    not done13

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 const express = require('express');
-
+const path = require('path'); // Make sure to include path
 const app = express();
-const port = process.env.PORT || 3002;
 
-app.use(express.json());
-
-// Configure EJS
+// Set EJS as the view engine
 app.set('view engine', 'ejs');
+console.log('Views directory:', path.join(__dirname, 'views'));
+
+app.set('views', path.join(__dirname, 'views')); // Explicitly set views directory
+
 app.get('/', (req, res) => {
-    res.render('index.ejs'); // Ensure you provide the correct extension
+    res.render('index'); // Renders views/index.ejs
 });
 
-
-// app.get('/', (req, res) => {
-//     res.send('Hello, World!');
-// });
-
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
+app.listen(3002, () => {
+    console.log('Server is running on port 3000');
 });
